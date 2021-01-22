7279b169fa29a9dd82e3421b96c08214b0556aa2
Narek085 <knarek085@gmail.com>
2021-01-22 12:23:01 +0400

    create project initial docs

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int a = 6;
+        int b = 7;
+       System.out.print(a+b);
+       System.out.println("Hello world");
+
+        }
+    }
